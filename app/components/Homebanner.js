4c6cf6b3167c5d171a938a62ced54b42d100aac3
Kallol Ghosal <kallol@euronoxxgroup.com>
2024-09-11 10:54:10 +0530

    Home banner updated

--- app/components/Homebanner.js
+++ app/components/Homebanner.js
@@ -1,88 +1,114 @@
-import { Container } from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Link from "next/link";
+import Link from 'next/link';
+import React from 'react';
+import { Carousel, Container } from 'react-bootstrap';
 import '../globals.css';
-import { Carousel } from "react-bootstrap";
 
-const homebanner = () => {
-
-    return (
-        <Container fluid className="p-0">
-            <div className="headbar">
-                <Container className="py-2">
-                    Enjoy Amazing SIM Only Deals From the Animal <span className="txtred">&amp;</span> Music Loving Network!
-                </Container>
+function Homebanner() {
+  return (
+    <>
+    <Container fluid className="p-0">
+        <div className="headbar">
+            <Container className="py-2">
+                Enjoy Amazing SIM Only Deals From the Animal <span className="txtred">&amp;</span> Music Loving Network!
+            </Container>
+        </div>
+    </Container>
+    <div>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="/img/banner-img-1.webp"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <div className="btnbox">
+                <Link href='/zoiko-mobile-switch-save-form' className="gr">Switch &amp; Save</Link>
+                <Link href="#" className="wh">Find out more</Link>
             </div>
-
-            <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
-                <div className="carousel-inner">
-                    <div className="carousel-item slide1 active">
-                        <div className="container">
-                            <div className="btnbox">
-                                <Link href='/zoiko-mobile-switch-save-form' className="gr">Switch &amp; Save</Link>
-                                <Link href="#" className="wh">Find out more</Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="carousel-item slide2">
-                        <div className="container">
-                            <div className="btnbox">
-                                <Link href='/zoiko-mobile-switch-save-form' className="gr">Switch &amp; Save</Link>
-                                <Link href="#" className="wh">Find out more</Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="carousel-item slide3">
-                        <div className="container">
-                            <div className="btnbox">
-                                <Link href='/zoiko-mobile-switch-save-form' className="gr">Switch &amp; Save</Link>
-                                <Link href="#" className="wh">Find out more</Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="carousel-item slide4">
-                        <div className="container">
-                            <div className="btnbox">
-                                <Link href="#" className="gr">View Detail</Link>
-                                <Link href="#" className="wh">Buy Now</Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="carousel-item slide5">
-                        <div className="container">
-                            <div className="btnbox">
-                                <Link href='/zoiko-mobile-switch-save-form' className="gr">Switch &amp; Save</Link>
-                                <Link href="#" className="wh">Find out more</Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="carousel-item slide6">
-                        <div className="container">
-                            <div className="btnbox">
-                                <Link href='/zoiko-mobile-switch-save-form' className="gr">Switch &amp; Save</Link>
-                                <Link href="#" className="wh">Find out more</Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="carousel-item slide7">
-                        <div className="container">
-                            <div className="btnbox">
-                                <Link href='/zoiko-mobile-switch-save-form' className="gr">Switch &amp; Save</Link>
-                                <Link href="#" className="wh">Find out more</Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Previous</span>
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Next</span>
-                </button>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="/img/banner-img-2.webp"
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <div className="btnbox">
+                <Link href='/zoiko-mobile-switch-save-form' className="gr">Switch &amp; Save</Link>
+                <Link href="#" className="wh">Find out more</Link>
             </div>
-        </Container>
-    );
-};
-export default homebanner;
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="/img/banner-img-3.webp"
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <div className="btnbox">
+                <Link href='/zoiko-mobile-switch-save-form' className="gr">Switch &amp; Save</Link>
+                <Link href="#" className="wh">Find out more</Link>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="/img/slide-4.png"
+            alt="Fourth slide"
+          />
+          <Carousel.Caption>
+            <div className="btnbox">
+                <Link href="#" className="gr">View Detail</Link>
+                <Link href="#" className="wh">Buy Now</Link>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="/img/slide-5.webp"
+            alt="Fifth slide"
+          />
+          <Carousel.Caption>
+            <div className="btnbox">
+                <Link href='/zoiko-mobile-switch-save-form' className="gr">Switch &amp; Save</Link>
+                <Link href="#" className="wh">Find out more</Link>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="/img/slide-6.webp"
+            alt="Sixth slide"
+          />
+          <Carousel.Caption>
+            <div className="btnbox">
+                <Link href='/zoiko-mobile-switch-save-form' className="gr">Switch &amp; Save</Link>
+                <Link href="#" className="wh">Find out more</Link>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="/img/slide-7.webp"
+            alt="Seventh slide"
+          />
+          <Carousel.Caption>
+            <div className="btnbox">
+                <Link href='/zoiko-mobile-switch-save-form' className="gr">Switch &amp; Save</Link>
+                <Link href="#" className="wh">Find out more</Link>
+            </div>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+    </>
+  );
+}
+export default Homebanner;
