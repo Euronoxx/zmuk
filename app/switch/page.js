e3fe0b6dcd02590d400c14d42379ec184d206e93
Kallol Ghosal <kallol@euronoxxgroup.com>
2024-10-24 18:23:57 +0530

    Switch page updated

--- app/switch/page.js
+++ app/switch/page.js
@@ -9,8 +9,8 @@ export default function Switch () {
         <>
         <Header />
         <HeadBar text={<>Switch <span style={{color:'#DF1E5A'}}>&amp;</span> Save to Zoiko Mobile: Value Meets Exceptional Service</>} />
-        <Container className="blackbordercontainer my-5 p-3">
-            <div className="p-2 body22">
+        <Container className="blackbordercontainer my-5 p-4">
+            <div className="py-2 body22">
             Welcome to Zoiko Mobile! Our &quot;Switch &amp; Save&quot; initiative is redefining mobile connectivity for both individuals and businesses. If you&apos;re looking for a mobile plan that offers more for less, you&apos;re in the right place. Our plans are designed to provide significant savings, extensive data allowances, and comprehensive coverage, ensuring you stay connected in every way that matters.
             </div>
             <h2 className="bigred">Making the Switch is as Simple as 1, 2, 3</h2>
@@ -19,7 +19,7 @@ export default function Switch () {
                 <li><span className="txtred">Compare and Discover the Zoiko Advantage:</span> Check out our comparison section right here to see how our offerings stack up against your current plan. Spoiler alert: you&apos;ll be impressed! Click here to compare plans.</li>
                 <li><span className="txtred">Switch Effortlessly Online:</span> Use our straightforward online tool to make the switch. From handling your number transfer with a PAC code to finalising your plan choice, everything is streamlined for your convenience.</li>
             </ol>
-            <div className="p-2 body22">
+            <div className="py-2 body22">
                 <span className="bigred">Why Choose Zoiko Mobile?</span><br />
                 <span className="txtred">Guaranteed Savings:</span> Find a comparable plan for less elsewhere? We&apos;ll match the difference for the duration of your current contract, ensuring you&apos;re always getting the best deal.<br />
                 <span className="txtred">Unbeatable Value:</span> Our plans are up to 400% more cost-effective than major competitors, including Vodafone, Three, and Tesco Mobile, without compromising on quality or service.<br />
@@ -27,7 +27,7 @@ export default function Switch () {
                 <span className="txtred">Global Connectivity Without Extra Charges:</span> Unlike some competitors, we offer up to 250 free international minutes, making it easier to stay connected with loved ones or business contacts abroad.<br />
                 <span className="txtred">Unlimited Data Plans:</span> With our Unlimited plan, save over competitors like Vodafone, plus enjoy 250 free international minutes.<br />
             </div>
-            <div className="p-2 body22">
+            <div className="py-2 body22">
                 <span className="bigred">Comparative Insights: Your Business and Personal Needs Addressed</span><br />
                 Zoiko Mobile stands out with its competitive pricing and comprehensive plans tailored for both individual and business use:<br />
                 <span className="txtred">Business Plans Compared:</span> Our business plans, such as the 30GB package at just £11.99, offer more value and savings compared to similar plans from Vodafone UK and Tesco Mobile.<br />
@@ -36,11 +36,11 @@ export default function Switch () {
                 <span className="txtred">Testimonials and Customer Reviews:</span> Don&apos;t just take our word for it. Read what our satisfied customers say about the difference Zoiko Mobile has made in their connectivity and savings.<br />
                 <span className="txtred">Environmental and Social Commitments:</span> Choosing Zoiko Mobile is a step towards supporting sustainable and ethical mobile services. We&apos;re committed to reducing our environmental impact and supporting community initiatives.<br />
             </div>
-            <div className="p-2 body22">
+            <div className="py-2 body22">
                 <span className="bigred">Seize the Zoiko Advantage Now</span><br />
                 Begin your journey to enhanced connectivity and savings today. Explore our plans, compare with your current service, and switch online effortlessly - all right here on our website! Zoiko Mobile not only promises but delivers unmatched value and service.
             </div>
-            <div className="p-2 body22">
+            <div className="py-2 body22">
                 <span className="bigred">Ready to Switch?</span><br />
                 Great! Click the button below to get started with our hassle-free online switching tool. We&apos;ll handle your number transfer with a PAC code if needed. Click here to switch online: link to your CRM switching tool Welcome to Zoiko Mobile - Connecting Every Possibility. The smart choice for those who demand more from their mobile service.
             </div>
