10d2ffe00b4649d54b6dcf9076363bef09f8f45a
Kallol Ghosal <kallol@euronoxxgroup.com>
2024-11-26 16:12:28 +0530

    Activate form validation updated

--- app/activate/page.js
+++ app/activate/page.js
@@ -1,39 +1,96 @@
 "use client"
-import { Container, Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Row, Col, Form } from "react-bootstrap";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import HeadBar from "../components/HeadBar";
 
 const activate = () => {
+
+    const [errors, setErrors] = useState({});
+    const [selectedValue, setSelectedValue] = useState('');
+    const [formData, setFormData] = useState({
+        username: "",
+        email: "",
+        otpcode: "",
+        serialno: "",
+        title: "",
+        lname: "",
+        fname: "",
+        dob: "",
+        package: "",
+        country: "",
+        zipcode: "",
+        city: "",
+        addr: ""
+    });
+    const handleChange = (e) => {
+        setSelectedValue(e.target.value);
+        const { name, value, type, checked } = e.target
+        setFormData({
+          ...formData,
+          [name]: type === "checkbox" ? checked : value,
+        })
+    }
+    const validate = () => {
+        let formErrors = {}
+    
+        if (!formData.username) formErrors.username = "Username is required"
+        if (!formData.dob) formErrors.dob = "DOB is required"
+        if (!formData.otpcode) formErrors.otpcode = "OTP Code is required"
+        if (!formData.serialno) formErrors.serialno = "Serial no is required"
+        if (!formData.lname) formErrors.lname = "Last name is required"
+        if (!formData.fname) formErrors.fname = "First name is required"
+        if (!formData.email) {
+          formErrors.email = "Email is required"
+        } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+          formErrors.email = "Email address is invalid"
+        }
+    
+        setErrors(formErrors)
+        return Object.keys(formErrors).length === 0
+    }
+
+    const onSubmit  = async (e) => {
+        e.preventDefault()
+        if (!validate()) return
+        console.log(formData);
+    }
+
     return (
         <>
         <Header />
         <HeadBar text='Activate your SIM' />
         <Container>
-            <h4 className="my-4 green18 text-center">Get started with our services! Initiate the activation process for your new SIM card here</h4>
-            <form action="" method="post" className="my-5">
+            <h4 className="my-4 green18 text-center">Get started with our services! Initiate the activation process htmoFor your new SIM card here</h4>
+            <Form onSubmit={onSubmit} className="my-5">
                 <Row>
                     <Col>
-                        <label for="fname">Username</label>
-                        <input type="text" name="username" className="form-control" placeholder="Username" aria-label="Username" required />
+                        <label htmoFor="username">Username</label>
+                        <input type="text" name="username" className="form-control" onChange={handleChange} value={formData.username} />
+                        {errors.username && <p className="txtred">{errors.username}</p>}
                     </Col>
                     <Col>
-                        <label for="lname">Email</label>
-                        <input type="text" name="email" className="form-control" placeholder="Email" aria-label="Email" required />
+                        <label htmoFor="email">Email</label>
+                        <input type="text" name="email" className="form-control" onChange={handleChange} value={formData.email} />
+                        {errors.email && <p className="txtred">{errors.email}</p>}
+                    </Col>
+                    <Col>
+                        <label htmoFor="otpcode">OTP Code</label>
+                        <input type="text" name="otpcode" className="form-control" onChange={handleChange} value={formData.otpcode} />
+                        {errors.otpcode && <p className="txtred">{errors.otpcode}</p>}
                     </Col>
                 </Row>
                 <br />
-                <label for="data">OTP Code</label>
-                <input type="text" name="otpcode" className="form-control" placeholder="OTP Code" aria-label="OTP Code" />
-                <br />
                 <Row>
                     <Col>
-                        <label for="email">SIM Serial Number</label>
-                        <input type="email" name="serialno" className="form-control" placeholder="SIM Serial Number" aria-label="SIM Serial Number" required />
+                        <label htmoFor="serialno">SIM Serial Number</label>
+                        <input type="text" name="serialno" className="form-control" onChange={handleChange} value={formData.serialno} />
+                        {errors.serialno && <p className="txtred">{errors.serialno}</p>}
                     </Col>
                     <Col>
-                        <label for="mobile">Title</label>
-                        <select className="form-select" name="title">
+                        <label htmoFor="title">Title</label>
+                        <select className="form-select" name="title" onChange={handleChange} value={formData.title}>
                             <option value={'Mr'}>Mr</option>
                             <option value={'Mrs'}>Mrs</option>
                             <option value={'Miss'}>Miss</option>
@@ -46,23 +103,26 @@ const activate = () => {
                 <br />
                 <Row>
                     <Col>
-                        <label for="provider">Last Name</label>
-                        <input type="text" name="lname" className="form-control" placeholder="Last Name" aria-label="Last Name" />
+                        <label htmoFor="lname">Last Name</label>
+                        <input type="text" name="lname" className="form-control" onChange={handleChange} value={formData.lname} />
+                        {errors.lname && <p className="txtred">{errors.lname}</p>}
                     </Col>
                     <Col>
-                        <label for="cost">First Name</label>
-                        <input type="text" name="fname" className="form-control" placeholder="First Name" aria-label="First Name" />
+                        <label htmoFor="fname">First Name</label>
+                        <input type="text" name="fname" className="form-control" onChange={handleChange} value={formData.fname} />
+                        {errors.fname && <p className="txtred">{errors.fname}</p>}
                     </Col>
                 </Row>
                 <br />
                 <Row>
                     <Col>
-                        <label for="email">Date of Birth</label>
-                        <input type="date" name="dob" className="form-control" placeholder="Date of Birth" aria-label="Date of Birth" required />
+                        <label htmoFor="dob">Date of Birth</label>
+                        <input type="date" name="dob" className="form-control" onChange={handleChange} value={formData.dob} />
+                        {errors.dob && <p className="txtred">{errors.dob}</p>}
                     </Col>
                     <Col>
-                        <label for="mobile">Your Zoiko Package</label>
-                        <select className="form-select" name="package">
+                        <label htmoFor="package">Your Zoiko Package</label>
+                        <select className="form-select" name="package" onChange={handleChange} value={formData.package}>
                             <option value="77984">Zoiko Saver Basic</option>
                             <option value="38890">Business Booster Pro</option>
                             <option value="38872">Business Booster Premium</option>
@@ -87,28 +147,28 @@ const activate = () => {
                 <br />
                 <Row>
                     <Col>
-                        <label for="fname">Country</label>
-                        <input type="text" name="country" className="form-control" placeholder="Country" aria-label="Country" required />
+                        <label htmoFor="country">Country</label>
+                        <input type="text" name="country" className="form-control" onChange={handleChange} value={formData.country} />
                     </Col>
                     <Col>
-                        <label for="lname">Postalcode/ZIP code</label>
-                        <input type="text" name="zipcode" className="form-control" placeholder="Postalcode/ZIP code" aria-label="Postalcode/ZIP code" required />
+                        <label htmoFor="zipcode">Postalcode/ZIP code</label>
+                        <input type="text" name="zipcode" className="form-control" onChange={handleChange} value={formData.zipcode} />
                     </Col>
                 </Row>
                 <br />
                 <Row>
                     <Col>
-                        <label for="fname">City</label>
-                        <input type="text" name="city" className="form-control" placeholder="City" aria-label="City" required />
+                        <label htmoFor="city">City</label>
+                        <input type="text" name="city" className="form-control" onChange={handleChange} value={formData.city} />
                     </Col>
                     <Col>
-                        <label for="lname">Address</label>
-                        <input type="text" name="addr" className="form-control" placeholder="Address" aria-label="Address" required />
+                        <label htmoFor="addr">Address</label>
+                        <input type="text" name="addr" className="form-control" onChange={handleChange} value={formData.addr} />
                     </Col>
                 </Row>
                 <br />
                 <input type="submit" value="Submit" name="submit" className="btn btn-outline-danger btn-lg" />
-            </form>
+            </Form>
         </Container>
         <Footer />
         </>
---
+++ app/how-to-activate-sim/layout.js
@@ -0,0 +1,19 @@
+
+export const metadata = {
+    title: "Zoiko Mobile | How To Activate SIM",
+    description: "Zoiko Mobile - How To Activate SIM",
+    metadataBase: new URL("https://zoikomobile.co.uk"),
+    alternates: {
+        canonical: "/activate",
+    },
+    openGraph: {
+        title: "Zoiko Mobile | How To Activate SIM",
+        description: "Zoiko Mobile - How To Activate SIM",
+        url: "/activate",
+        images: "/img/zoikomobile_logo.png",
+    },
+};
+  
+export default function RootLayout({ children }) {
+    return <>{children}</>;
+}
